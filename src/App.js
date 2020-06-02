51db1721c961518e7b472dc38ad14eba2985a30d
Missy Moreno <missycodes247@gmail.com>
2020-06-02 14:53:18 -0400

    connect succesfully to OpenWeater API. program created through security point of tutorial

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import Weather from './Weather.js';
+// import Weather from "./components/Weather/Weather";
+import Forecast from "./components/Forecast/Forecast";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
 
-        <Weather/>
-
-        </a>
-      </header>
+        <Forecast/>
+        
     </div>
   );
 }
