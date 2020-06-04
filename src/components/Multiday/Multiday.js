c032e3291391ebba54081ff64ba9f457d86627d3
Juan Jose Fernandez <fernandez.juan.j@gmail.com>
2020-06-04 15:27:03 -0400

    Uses 7 day forecast API callsuccessfully. Requires tinkering to work with the existing form, still. There are 2 fetches, where it should end up with only 1.

--- src/components/Multiday/Multiday.js
+++ src/components/Multiday/Multiday.js
@@ -15,47 +15,24 @@ const Multiday = () => {
 
     function getMultiDay(e) {
 
-    function weatherBalloon( cityID ) {
-        var key = '{yourkey}';
-        fetch('https://api.openweathermap.org/data/2.5/forecast?id=' + cityID+ '&appid=' + "de21f1eaf5bf29f1eb059f7f97f70b23")  
-        .then(function(resp) { return resp.json() }) // Convert data to json
-        .then(function(data) {
-          console.log(data);
-        })
-        .catch(function() {
-          // catch any errors
-        });
-      }
+        function weatherBalloon( cityID ) {
+            var key = '{yourkey}';
+            fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=minutely,hourly&units=imperial&appid=de21f1eaf5bf29f1eb059f7f97f70b23')  
+            .then(function(resp) { return resp.json() }) // Convert data to json
+            .then(function(data) {
+            console.log(JSON.stringify(data.current.temp));
+            })
+            .catch(function() {
+            // catch any errors
+            });
+        }
 
-      console.log("before weather balloon");
-      weatherBalloon( 6167865 );
-      console.log("after weather balloon");
-      
+        console.log("before weather balloon");
+        weatherBalloon( 6167865 );
+        console.log("after weather balloon");
+        
         e.preventDefault();
      
-
-
-        // For testing the basic API code provided by RapidAPI
-        fetch("https://community-open-weather-map.p.rapidapi.com/forecast?q=san%20francisco%252Cus", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-                "x-rapidapi-key": "cb7921a5aemsh82dc0943cf8aed3p1387ffjsn317a83d3b4cd",
-                "Access-Control-Allow-Origin": "*"
-            }
-        })
-        .then(response => {
-            console.log(response);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-        // 
-        if (city.length === 0) {
-            console.log("In check for city lenght of 0");
-
-            return setError(true);
-        }
 
         // Clear state in preparation for new data
         setError(false);
