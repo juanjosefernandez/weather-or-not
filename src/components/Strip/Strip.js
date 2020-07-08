ab35a94f2518b0333fc4007f25240923f1412b76
Juan Jose Fernandez <fernandez.juan.j@gmail.com>
2020-07-07 20:16:45 -0400

    Cleans up the UI to only show the contents of the strips when there has been a successful API request

--- src/components/Strip/Strip.js
+++ src/components/Strip/Strip.js
@@ -13,12 +13,14 @@ const Strip = (props) => {
                 {/* <p>Hi Temperature, Lo Temp, Description, Day of week and date:</p> */}
                 {/* where we use the data from the API call, only render it IF the API call has been successful */}
                 {/* <p>Safe/Unsafe: {props.safe ? 'safe' : 'unsafe'} </p> */}
-                <p>Lo: {props.safe ?  JSON.stringify(props.responseObj.daily[props.day].temp.min) + "°F" : ""}</p>
-                <p>Hi: {props.safe ?  JSON.stringify(props.responseObj.daily[props.day].temp.max) + "°F"  : ""}</p>
-                <p>Main Description: {props.safe ?  JSON.stringify(props.responseObj.daily[props.day].weather[0].main) : ""}</p>
-                <p>Sub Description: {props.safe ?  JSON.stringify(props.responseObj.daily[props.day].weather[0].description) : ""}</p>
+                <p>{props.safe ?  "Lo:" + JSON.stringify(props.responseObj.daily[props.day].temp.min) + "°F" : ""}</p>
+                <p>{props.safe ? "Hi:" + JSON.stringify(props.responseObj.daily[props.day].temp.max) + "°F"  : ""}</p>
+                <p>{props.safe ?  "Main Description:" + JSON.stringify(props.responseObj.daily[props.day].weather[0].main) : ""}</p>
+                <p>{props.safe ?  "Sub Description: " + JSON.stringify(props.responseObj.daily[props.day].weather[0].description) : ""}</p>
                 {/* <p>Icon ID: {iconId}</p> */}
-                <img src={"http://openweathermap.org/img/wn/"+ iconId + "@2x.png"}></img>
+                <div>
+                    {props.safe ? <img src={"http://openweathermap.org/img/wn/"+ iconId + "@2x.png"}></img> : ""}
+                </div>
                 {/* {props.safe ?  JSON.stringify(props.responseObj.daily[props.day].weather[0].icon) : ""} */}
                 
                 </section>
